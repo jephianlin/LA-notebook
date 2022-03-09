b53d40cbb73b30cbd8f361b34793c5f6553d7920
Jephian Lin <jephianlin@gmail.com>
2022-03-09 22:55:57 +0800

    update sym.py, 607 fix typo

--- sym.py
+++ sym.py
@@ -1,3 +1,24 @@
+from sage.matrix.constructor import matrix
+from sage.matrix.special import zero_matrix
+
+def sym_from_list(n, entries):
+    """
+    Input:
+        n: integer, size of the symmetric matrix
+        entries: list, entries of the symmetric matrix in row major order
+    Output:
+        the symmetric matrix
+    """
+    k = 0
+    B = matrix([entries])
+    A = zero_matrix(n,n).change_ring(B.base_ring())
+    for i in range(n):
+        for j in range(i,n):
+            A[i,j] = entries[k]
+            A[j,i] = entries[k]
+            k += 1
+    return A
+
 def inertia(A):
     if A != A.transpose():
         raise TypeError("A must be symmetric.")
